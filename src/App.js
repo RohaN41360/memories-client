83ef4297b25a026846e06b2d2505ba337e5f94cb
Rohan <>
2023-07-25 18:28:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
      
       <Routes>
+      <Route path="/" element={<Feed />} />
         <Route path="/upload" element={<UploadForm />} />
         <Route path="/feed" element={<Feed />} />
       </Routes>
